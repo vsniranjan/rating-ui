8cb2d216d4b48b642a0bd8c95f46a7b630893251
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-28 19:29:40 +0530

    Learning states

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
 
   const clicked = (index) => console.log(index);
@@ -6,8 +11,16 @@ const Rating = () => {
     <div className='rating-container'>
       <h2>Rate your experience</h2>
       <div className='stars'>
+        <p>{hover}</p>
+        <p>{rating}</p>
         {stars.map((star, index) => (
-          <span onClick={() => clicked(index)} key={star} className='star'>
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className='star'
+          >
             {"\u2605"}
           </span>
         ))}
