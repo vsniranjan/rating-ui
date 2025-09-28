e6ecebb7fa5755a8490e33a35a5e38d5c4f51bb0
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-28 22:06:27 +0530

    Learning about props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Rating from "./components/Rating";
 const App = () => {
   return (
-    <div>
+    <div className='container'>
       <Rating />
+      <Rating question='How is you experience learning JS?' />
+      <Rating
+        question='What are you opinions on Pineaple on Pizza'
+        feedbackMessages={[
+          "Hate it",
+          "Dislike it",
+          "Meh",
+          "Like it",
+          "Love it",
+        ]}
+      />
     </div>
   );
 };
--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
 
-const Rating = () => {
+const Rating = ({
+  question = "Rate your experience",
+  feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"],
+}) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   const stars = Array.from({ length: 5 }, (_, i) => i + 1);
-  const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
 
   return (
     <div className='rating-container'>
-      <h2>Rate your experience</h2>
+      <h2>{question}</h2>
       <div className='stars'>
         {stars.map((star) => (
           <span
