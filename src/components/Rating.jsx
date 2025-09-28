54811be115da85aa30a229f291cd07766283410f
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-28 18:48:30 +0530

    Event Handling

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,10 +1,13 @@
 const Rating = () => {
+  const stars = Array.from({ length: 5 }, (_, i) => i + 1);
+
+  const clicked = (index) => console.log(index);
   return (
     <div className='rating-container'>
       <h2>Rate your experience</h2>
       <div className='stars'>
-        {[1, 2, 3, 4, 5].map((star) => (
-          <span key={star} className='star'>
+        {stars.map((star, index) => (
+          <span onClick={() => clicked(index)} key={star} className='star'>
             {"\u2605"}
           </span>
         ))}
