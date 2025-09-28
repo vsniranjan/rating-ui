5e00452d563b1e53435cad4c469411d84ea68388
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-28 17:20:00 +0530

    Lists in react

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,16 +2,15 @@ const Rating = () => {
   return (
     <div className='rating-container'>
       <h2>Rate your experience</h2>
+      <div className='stars'>
+        {[1, 2, 3, 4, 5].map((star) => (
+          <span key={star} className='star'>
+            {"\u2605"}
+          </span>
+        ))}
+      </div>
     </div>
   );
-};
-
-const styles = {
-  conatiner: {
-    textAlign: "center",
-    fontFamily: "Arial",
-    padding: "20px",
-  },
 };
 
 export default Rating;
