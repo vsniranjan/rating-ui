4df0e7c08dcc7f22e85dff80672aaadf05df37d4
Niranjan V S <niranjan.veluthamessery@gmail.com>
2025-09-28 17:09:00 +0530

    Styling in React

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,17 @@
 const Rating = () => {
-  return <div>Rate your experience</div>;
+  return (
+    <div className='rating-container'>
+      <h2>Rate your experience</h2>
+    </div>
+  );
+};
+
+const styles = {
+  conatiner: {
+    textAlign: "center",
+    fontFamily: "Arial",
+    padding: "20px",
+  },
 };
 
 export default Rating;
